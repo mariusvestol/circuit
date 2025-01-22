c0d1d32745da88a6edc0c373fc06e5fd2f6ccacd
Marius Vestol <marves@stud.ntnu.no>
2025-01-22 18:13:43 +0100

    mordi

---
+++ Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Last inn FXML-filen
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("test.fxml"));
+        Parent root = loader.load();
+
+        // Sett opp scenen
+        Scene scene = new Scene(root);
+
+        // Konfigurer og vis vinduet
+        primaryStage.setTitle("Elektrisk Krets Program");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
